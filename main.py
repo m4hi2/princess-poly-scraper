ba4aafc47564fd2a9a67f17b70649ef43b23810c
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2022-02-03 02:22:09 +0600

    fix: base product name
    didn't work becasue case missmatch

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ page = browser.new_page()
 # ?currency=BDT because the page was refreshing and playwright was getting timmedout on the page
 page.goto("https://us.princesspolly.com/products/the-soho-heels-beige?currency=BDT")
 color = page.query_selector(".product__active-color-value").inner_text().lower()
-product_name = page.query_selector(".product__title").inner_text().replace(color, "").strip().lower()
+product_name = page.query_selector(".product__title").inner_text().lower().replace(color, "").strip()
 variants_options_elements = page.query_selector_all("#SingleOptionSelector-0>option")
 
 variants_data = {v.get_attribute("value"): v.get_attribute("data-stock") for v in variants_options_elements if v.get_attribute("data-stock")}
