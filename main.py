167bbf8bae58bdc966cf256a80dec113c4283f2d
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2022-02-03 03:00:56 +0600

    chore: trying scraper logic on different product

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ browser = p.chromium.launch(headless=False)
 page = browser.new_page()
 
 # ?currency=BDT because the page was refreshing and playwright was getting timmedout on the page
-page.goto("https://us.princesspolly.com/products/the-soho-heels-beige?currency=BDT")
+page.goto("https://us.princesspolly.com/products/huxley-set-black?currency=BDT")
 color = page.query_selector(".product__active-color-value").inner_text().lower()
 product_name = page.query_selector(".product__title").inner_text().lower().replace(color, "").strip()
 variants_options_elements = page.query_selector_all("#SingleOptionSelector-0>option")
