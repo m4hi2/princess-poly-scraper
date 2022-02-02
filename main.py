0ed459b896b339a1c901314fcda1d94c1407df11
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2022-02-03 02:58:05 +0600

    feat: product price extraction

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from locale import currency
+from traceback import print_tb
 from playwright.sync_api import sync_playwright
 
 p = sync_playwright().start()
@@ -10,6 +11,11 @@ page.goto("https://us.princesspolly.com/products/the-soho-heels-beige?currency=B
 color = page.query_selector(".product__active-color-value").inner_text().lower()
 product_name = page.query_selector(".product__title").inner_text().lower().replace(color, "").strip()
 variants_options_elements = page.query_selector_all("#SingleOptionSelector-0>option")
+current_price =  page.query_selector("span[data-product-price]").inner_text().replace("$", "")
+original_price = page.query_selector("s[data-compare-price]").inner_text().replace("$", "")
+
+if original_price == "":
+    original_price = current_price
 
 variants_data = {v.get_attribute("value"): v.get_attribute("data-stock") for v in variants_options_elements if v.get_attribute("data-stock")}
 
@@ -17,3 +23,5 @@ variants_data = {v.get_attribute("value"): v.get_attribute("data-stock") for v i
 print(color)
 print(product_name)
 print(variants_data)
+print(current_price)
+print(original_price)
