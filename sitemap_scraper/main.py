1ee322b9218e07ad6c602fc8350441cb18b86de1
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2022-02-04 00:34:59 +0600

    feat: product link extraction from sitemaps

--- sitemap_scraper/main.py
+++ sitemap_scraper/main.py
@@ -1,4 +1,15 @@
 import re
+import requests
+
+main_sitemap = "https://us.princesspolly.com/sitemap.xml"
+
+def main():
+    sitemap_response = requests.get(main_sitemap)
+    product_sitemap_links = find_product_links(sitemap_response.text)
+    for product_sitemap_link in product_sitemap_links:
+        product_response = requests.get(product_sitemap_link)
+        product_links = find_product_links(product_response.text)
+        print(product_links)
 
 def find_product_links(xml_file: str) -> list:
     """Finds links related to products in the sitemap.xml file.
@@ -12,3 +23,6 @@ def find_product_links(xml_file: str) -> list:
     # Didn't want to use BeautifulSoup, so I just used regex to find the links
     links = re.findall('<loc>(.*?)</loc>', xml_file)
     return [link for link in links if "products" in link]
+
+if __name__ == "__main__":
+    main()
